8a0f8a34f60bf89d41eee44b1b92fbaf431ce204
Elihuso Quigley <Elihuso@outlook.com>
2024-06-07 23:20:05 +0800

    [T] Try to patch github pages

---
+++ scripts/github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i 's/"\/assets\/"/"\/MainPage\/assets\/"/g ./dist/index.html
+sed -i 's/"\/assets\/"/"\/MainPage\/assets\/"/g ./dist/*.js
+sed -i 's/"\/assets\/"/"\/MainPage\/assets\/"/g ./dist/*.css
